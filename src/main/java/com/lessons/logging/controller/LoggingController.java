596ed9dfd6736639669b24d8fa26f31c3f834d8c
Sylvester Musyoki <sylvester.musyoki@cellulant.com>
2019-05-23 13:09:28 +0300

    initial Commit

---
+++ src/main/java/com/lessons/logging/controller/LoggingController.java
@@ -0,0 +1,11 @@
+package com.lessons.logging.controller;
+
+/**
+ * Created by  IntelliJ IDEA.
+ * Project logging
+ * User: Sylvester Musyoki
+ * Date: 5/23/19
+ * Time: 1:05 PM
+ */
+public class LoggingController {
+}
